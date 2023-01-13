d828eeb2972b77d16c44a14ef16c20fa1e84a86a
ahm-rubayed <rubayedahmed04@gmail.com>
2023-01-13 16:10:27 +0600

    added searchbar in top navbar, fixed card color

--- src/pages/home/Banner/Banner.js
+++ src/pages/home/Banner/Banner.js
@@ -4,18 +4,20 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className=''>
+        <div className='pt-12'>
             <div className="hero min-h-screen banner">
-  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-2xl">
     <p className='mb-3'>All You Need To Know</p>
       <h1 className="mb-5 text-6xl ">We Provide Skill Based <br /> Online Class</h1>
       <p className="mb-5 mt-7">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn border-none px-8 mt-3 bg-cyan-500 rounded-full">GET STARTED</button>
+      <button className="btn border-none text-white px-8 mt-3 bg-primary rounded-full">GET STARTED</button>
     </div>
   </div>
 </div>
         </div>
     );
 };
+
+export default Banner;
