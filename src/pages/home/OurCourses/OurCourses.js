426d7f4a066d6a0da320a1a9f18d7f6d37a69e2c
neasher <neasher75@gmail.com>
2023-01-13 06:58:41 +0600

    testing

--- src/pages/home/OurCourses/OurCourses.js
+++ src/pages/home/OurCourses/OurCourses.js
@@ -70,30 +70,6 @@ const OurCourses = () => {
 
                 </div>
             </div>
-            <div className="card w-full rounded-tr-none bg-white-600 shadow-xl">
-                <figure>
-                    <img
-                        className="rounded-tl-2xl"
-                        src="https://placeimg.com/400/225/arch"
-                        alt="Shoes"
-                    />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title justify-center">Graphic Design</h2>
-                    <p className="text-center">
-                        We are experts in helping organizations leverage coaching to
-                        drive engagement, performance and{" "}
-                    </p>
-                    <div className="card-actions justify-center mt-2">
-                        <button className="bg-primary rounded-t-none rounded-b-none rounded-l-lg text-lg py-1 px-2 text-white">
-                            150$
-                        </button>
-                        <button className="rounded-r-xl bg-primary text-white text-lg py-1 px-3 ring-offset-2">
-                            Join Now
-                        </button>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 };
