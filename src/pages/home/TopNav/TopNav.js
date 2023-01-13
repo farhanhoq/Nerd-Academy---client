b63f76acf06f926272fc269148add5d57f7d72ff
ahm-rubayed <rubayedahmed04@gmail.com>
2023-01-13 15:31:27 +0600

    changed topbar  icon color and size

--- src/pages/home/TopNav/TopNav.js
+++ src/pages/home/TopNav/TopNav.js
@@ -6,22 +6,15 @@ const TopNav = () => {
     <div className="p-5">
       <div className ="flex justify-between items-center w-11/12 mx-auto">
         <div className="flex gap-x-3">
-           <p className= "cursor-pointer text-slate-400 hover:text-teal-500 text-xl" ><FaLinkedinIn /></p>
-           <p className= "cursor-pointer text-slate-400 hover:text-teal-500 text-xl" ><FaFacebookF /></p>
-           <p className= "cursor-pointer text-slate-400 hover:text-teal-500 text-xl" ><FaGoogle /></p>
-           <p className= "cursor-pointer text-slate-400 hover:text-teal-500 text-xl" ><FaTwitter /></p>
+           <p className= "cursor-pointer hover:text-primary text-lg" ><FaLinkedinIn /></p>
+           <p className= "cursor-pointer hover:text-primary text-lg" ><FaFacebookF /></p>
+           <p className= "cursor-pointer hover:text-primary text-lg" ><FaGoogle /></p>
+           <p className= "cursor-pointer hover:text-primary text-lg" ><FaTwitter /></p>
         </div>
-{/* ------------------------------- */}
-
-
 
 <div>
-<input type="text" placeholder="Search" className="input input-md rounded-full input-bordered" />
+<input type="text" placeholder="Search" className="input input-md rounded-full" />
 </div>
-
-
-
-{/* ------------------------------- */}
 
         <div>
             <ul className=" grid grid-cols-2 gap-x-4">
