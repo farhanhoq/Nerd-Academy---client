bd2e4be5e2222e2e4dd63dc909bd0e1d279ed55b
ahm-rubayed <rubayedahmed04@gmail.com>
2023-01-14 20:28:53 +0600

    m

--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -1,156 +1,43 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 // import nerd from '../../Assets/logo.jpg';
 
 const Navbar = () => {
+
+  const menuItems = <>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/">Services</Link></li>
+    <li><Link to="/">Page</Link></li>
+    <li><Link to="/">Courses</Link></li>
+    <li><Link to="/">News</Link></li>
+    <li><Link to="/">About</Link></li>
+    <li><Link to="/">Contact</Link></li>
+  </>
+
   return (
-      <nav className="navbar z-10 px-16">
+    <div>
+      <div className="navbar bg-base-100 w-11/12 mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a
-                  className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                  href="/"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                  href="/"
-                >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                  href="/"
-                >
-                  Page
-                </a>
-              </li>
-              <li>
-                <a
-                  className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                  href="/"
-                >
-                  Courses
-                </a>
-              </li>
-              <li>
-                <a
-                  className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                  href="/"
-                >
-                  News
-                </a>
-              </li>
-              <li>
-                <a
-                  className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                  href="/"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                  href="/"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              {menuItems}</ul>
           </div>
           {/* <img src={nerd} alt="" /> */}
-          <a href="/" className="font-bold">
-            <span className="italic text-3xl">Nerd</span>
-            <span className="text-cyan-500 text-2xl"> ACADEMY</span>
-          </a>
+          <Link href="/" className="font-bold"><span className="italic text-3xl">Nerd</span><span className="text-cyan-500 text-2xl"> ACADEMY</span></Link>
         </div>
-        <div className="navbar-end hidden lg:flex">
+        <div className="navbar-center hidden lg:flex">
           <ul className=" menu-horizontal gap-x-3">
-            <li>
-              <a
-                className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                href="/"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                href="/"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                href="/"
-              >
-                Page
-              </a>
-            </li>
-            <li>
-              <a
-                className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                href="/"
-              >
-                Courses
-              </a>
-            </li>
-            <li>
-              <a
-                className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                href="/"
-              >
-                News
-              </a>
-            </li>
-            <li>
-              <a
-                className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                href="/"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                className="p-2 hover:border-b-2 border-teal-500 cursor-pointer"
-                href="/"
-              >
-                Contact
-              </a>
-            </li>
+            {menuItems}
           </ul>
         </div>
-      </nav>
+        <div className="navbar-end">
+          {/* <a className="btn">Get started</a> */}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/pages/home/OurCourses/OurCourses.js
+++ src/pages/home/OurCourses/OurCourses.js
@@ -1,100 +1,77 @@
-import React from "react";
-import minusb from "../../../Assets/minusb.png";
-import book from "../../../Assets/book.png";
+import minusb from '../../../Assets/minusb.png'
+import book from '../../../Assets/book.png'
+
 
 const OurCourses = () => {
-  return (
-    <div className="my-20">
-      <h1 className="text-5xl text-center font-bold uppercase">our courses</h1>
 
-      <div className="grid justify-center">
-        <img className="h-full w-24" src={minusb} alt="" srcset="" />
-        <img className="w-12 mx-auto" src={book} alt="" srcset="" />
-      </div>
+    return (
+        <div>
+            <div className='my-24'>
+                <h1 className='text-5xl text-center font-bold '>OUR COURSES</h1>
 
-      <div className="">
-        <p className="text-center mt-6">
-          {" "}
-          Here are a few courses and more upcoming now just processing. Here are
-          a few courses and more upcoming now just processing.
-        </p>
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 container px-20 mt-12 sm:grid-cols-1 gap-6 my-10 mx-8 text-center">
-          <div className="card w-full rounded-tr-none bg-white-600 shadow-xl">
-            <figure>
-              <img
-                className="rounded-tl-2xl"
-                src="https://placeimg.com/400/225/arch"
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title justify-center">Graphic Design</h2>
-              <p className="text-center">
-                We are experts in helping organizations leverage coaching to
-                drive engagement, performance and{" "}
-              </p>
-              <div className="card-actions justify-center mt-2">
-                <button className="bg-primary rounded-t-none rounded-b-none rounded-l-lg text-lg py-1 px-2 text-white">
-                  150$
-                </button>
-                <button className="rounded-r-xl bg-primary text-white text-lg py-1 px-3 ring-offset-2">
-                  Join Now
-                </button>
-              </div>
+                <div className='grid justify-center'>
+                    <img className='h-full w-24' src={minusb} alt="" />
+                    <img className='h-full w-24' src={book} alt="" />
+                </div>
+
+                <div className=''>
+                    <p className='text-center mt-6'> Here are a few courses and more upcoming now just processing. <br />
+                        Here are a few courses and more upcoming now just processing.</p>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10 mx-8 text-center'>
+
+                        <div className="card w-full rounded-tr-none bg-white-600 shadow-xl">
+                            <figure>
+                                <img className='rounded-tl-2xl w-full' src="https://placeimg.com/400/225/arch" alt="Shoes" />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title justify-center">
+                                    Graphic Design
+                                </h2>
+                                <p className='text-center'>We are experts in helping organizations leverage coaching to drive engagement, performance and </p>
+                                <div className="card-actions justify-center mt-2">
+                                    <button className="bg-primary rounded-t-none rounded-b-none rounded-l-lg text-lg p-1 text-white">150$</button>
+                                    <button className="rounded-r-xl bg-primary text-white text-lg p-1 ring-offset-2">Join Now</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="card w-full rounded-tr-none bg-white-600 shadow-xl">
+                            <figure><img className='rounded-tl-2xl w-full' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title justify-center">
+                                    Graphic Design
+
+                                </h2>
+                                <p className='text-center'>We are experts in helping organizations leverage coaching to drive engagement, performance and </p>
+                                <div className="card-actions justify-center mt-2">
+                                    <div className="bg-cyan-600 rounded-t-none rounded-b-none rounded-l-lg text-lg p-1 text-white">150$</div>
+                                    <div className="rounded-r-xl bg-cyan-700 text-white text-lg p-1 ring-offset-2">Join Now</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card w-full rounded-tr-none bg-white-600 shadow-xl">
+                            <figure><img className='rounded-tl-2xl w-full' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title justify-center">
+                                    Graphic Design
+
+                                </h2>
+                                <p className='text-center'>We are experts in helping organizations leverage coaching to drive engagement, performance and </p>
+                                <div className="card-actions justify-center mt-2">
+                                    <div className="bg-cyan-600 rounded-t-none rounded-b-none rounded-l-lg text-lg p-1 text-white">150$</div>
+                                    <div className="rounded-r-xl bg-cyan-700 text-white text-lg p-1 ring-offset-2">Join Now</div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+                </div>
             </div>
-          </div>
-          <div className="card w-full rounded-tr-none bg-white-600 shadow-xl">
-            <figure>
-              <img
-                className="rounded-tl-2xl"
-                src="https://placeimg.com/400/225/arch"
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title justify-center">Graphic Design</h2>
-              <p className="text-center">
-                We are experts in helping organizations leverage coaching to
-                drive engagement, performance and{" "}
-              </p>
-              <div className="card-actions justify-center mt-2">
-                <button className="bg-primary rounded-t-none rounded-b-none rounded-l-lg text-lg py-1 px-2 text-white">
-                  150$
-                </button>
-                <button className="rounded-r-xl bg-primary text-white text-lg py-1 px-3 ring-offset-2">
-                  Join Now
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="card w-full rounded-tr-none bg-white-600 shadow-xl">
-            <figure>
-              <img
-                className="rounded-tl-2xl"
-                src="https://placeimg.com/400/225/arch"
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title justify-center">Graphic Design</h2>
-              <p className="text-center">
-                We are experts in helping organizations leverage coaching to
-                drive engagement, performance and{" "}
-              </p>
-              <div className="card-actions justify-center mt-2">
-                <button className="bg-primary rounded-t-none rounded-b-none rounded-l-lg text-lg py-1 px-2 text-white">
-                  150$
-                </button>
-                <button className="rounded-r-xl bg-primary text-white text-lg py-1 px-3 ring-offset-2">
-                  Join Now
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default OurCourses;
