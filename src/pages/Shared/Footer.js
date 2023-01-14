87711e9ae5a1541789aa516bfc05d61de9a09d77
ahm-rubayed <rubayedahmed04@gmail.com>
2023-01-14 12:38:01 +0600

    issue fixed in footer

--- src/pages/Shared/Footer.js
+++ src/pages/Shared/Footer.js
@@ -1,59 +1,71 @@
-import React from 'react';
-import { FaLinkedinIn , FaFacebookF , FaGoogle , FaTwitter} from 'react-icons/fa';
+import React from "react";
+import { FaLinkedinIn, FaFacebookF, FaGoogle, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
-    return (
-        <div>
-            <div className='w-11/12 mx-auto grid sm:grid-cols-2 md:grid-cols-4 my-16'>
-                <div className=''>
-                    <h1 className='text-2xl text-primary mb-4'>ABOUT US</h1>
-                    <div className='w-10/12'>
-                        <p>We are experts in helping organisations leverage coaching to drive engagement, </p>
-                        <p className='my-3 text-primary text-2xl'>001 5555 6666</p>
-                        <p>nerdacademy@gmail.com</p>
-                    </div>
-                </div>
-                <div>
-                    <h1 className='text-xl text-primary mb-4'>NAVIGATION</h1>
-                    <ul>
-                        <li className='my-2'>HOME</li>
-                        <li className='my-2'>COURSES</li>
-                        <li className='my-2'>NEWS</li>
-                        <li className='my-2'>ABOUT</li>
-                        <li className='my-2'>CONTACT</li>
-                    </ul>
-                </div>
-                <div>
-                    <h1 className='text-xl text-primary mb-4'>SERVICES</h1>
-                    
-                    <ul>
-                    <p className='my-2'>SERVICES</p>
-                        <p className='my-2'>COURSES</p>
-                        <p className='my-2'>OUR COURSES</p>
-                    </ul>
-
-                    
-                </div>
-                <div>
-                <ul className="flex gap-x-4">
-                    <li className= "cursor-pointer hover:text-primary text-xl transition duration-150 ease-in-out" ><FaLinkedinIn /></li>
-                    <li className= "cursor-pointer hover:text-primary text-xl transition duration-150 ease-in-out" ><FaFacebookF /></li>
-                    <li className= "cursor-pointer hover:text-primary text-xl transition duration-150 ease-in-out" ><FaGoogle /></li>
-                    <li className= "cursor-pointer hover:text-primary text-xl transition duration-150 ease-in-out" ><FaTwitter /></li>
-                </ul>
-                </div>
-            </div>
-            <div>
-            <footer className="footer items-center p-4 bg-primary text-center">
-  <div className="items-center text-white text-center mx-auto">
-    
-    <p>Copyright © 2023 - All right reserved by Nerd Academy</p>
-  </div> 
-  
-</footer>
-            </div>
+  return (
+    <div>
+            <div className="container mx-auto">
+        <hr/>
+      <div className="grid sm:grid-cols-2 md:grid-cols-5 my-16">
+        <div className="col-span-2">
+          <h2 className="text-2xl text-primary mb-4 uppercase font-semibold">About us</h2>
+          <div className="w-3/4">
+            <p className="text-slate-400">
+              We are experts in helping organisations leverage coaching to drive
+              engagement,{" "}
+            </p>
+            <p className="my-3 text-primary text-2xl">001 5555 6666</p>
+            <p>nerdacademy@gmail.com</p>
+          </div>
         </div>
-    );
+
+        <div className="justify-self-center">
+          <h2 className="text-xl text-primary mb-4 uppercase font-semibold">Navigation</h2>
+          <ul>
+            <li className="my-2">Home</li>
+            <li className="my-2">Courses</li>
+            <li className="my-2">News</li>
+            <li className="my-2">About</li>
+            <li className="my-2">Contact</li>
+          </ul>
+        </div>
+        <div className="justify-self-center">
+          <h2 className="text-xl text-primary mb-4 uppercase font-semibold">Serivces</h2>
+
+          <ul>
+            <li className="my-2">Services</li>
+            <li className="my-2">Courses</li>
+            <li className="my-2">Courses</li>
+          </ul>
+        </div>
+
+        <div className="justify-self-center">
+          <ul className="flex gap-x-4">
+            <li className="cursor-pointer hover:text-primary text-xl transition duration-150 ease-in-out">
+              <FaLinkedinIn />
+            </li>
+            <li className="cursor-pointer hover:text-primary text-xl transition duration-150 ease-in-out">
+              <FaFacebookF />
+            </li>
+            <li className="cursor-pointer hover:text-primary text-xl transition duration-150 ease-in-out">
+              <FaGoogle />
+            </li>
+            <li className="cursor-pointer hover:text-primary text-xl transition duration-150 ease-in-out">
+              <FaTwitter />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+      </div>
+    </div>
+            <footer className="footer items-center p-4 bg-primary text-center">
+            <div className="items-center text-white text-center mx-auto">
+              <p>Copyright © 2023 - All right reserved by Nerd Academy</p>
+            </div>
+          </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/home/Instructor/Instructor.js
+++ src/pages/home/Instructor/Instructor.js
@@ -3,7 +3,7 @@ import instructor from '../../../Assets/instructor.jpg'
 
 const Instructor = () => {
     return (
-        <div className='flex sm:flex-col md:flex-row items-center w-11/12 mx-auto shadow-xl my-36 rounded-xl'>
+        <div className='flex sm:flex-col md:flex-row items-center w-11/12 mx-auto shadow-2xl my-36 rounded-xl'>
 
             <div className='w-6/12'>
                 <img src={instructor} alt="" />
@@ -12,7 +12,7 @@ const Instructor = () => {
             <div className='w-6/12 p-5 ml-5'>
 
                 <h1 className='text-5xl font-bold mb-5'>Become an instructor</h1>
-                <div className='w-10/12 text-lg'>
+                <div className='w-10/12 text-lg text-slate-400'>
                     <p>Instructors from around the world teach millions of students on Nerd Academy. We provide the tools and skills to teach what you love.</p>
                 </div>
                 
