ea8a4731228f0ea91c313a8822d39e21f2180212
mehedi556 <mehe556di@gmail.com>
2023-01-15 01:23:23 +0600

    added new navbar

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -2,21 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer';
 import Navbar from '../pages/Shared/Navbar';
-import TopNav from "../pages/home/TopNav/TopNav"
+
 
 const Main = () => {
     return (
-        <div>
-            <div>
-                <TopNav></TopNav>
-            </div>
             <div>
                 <Navbar></Navbar>
                 <Outlet></Outlet>
                 <Footer></Footer>
             </div>
-
-        </div>
     );
 };
 
--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -1,40 +1,99 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import nerd from '../../Assets/logo.jpg';
+import { FaOpencart } from 'react-icons/fa';
 
 const Navbar = () => {
-
-  const menuItems = <>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/">Services</Link></li>
-    <li><Link to="/">Page</Link></li>
-    <li><Link to="/">Courses</Link></li>
-    <li><Link to="/">News</Link></li>
-    <li><Link to="/">About</Link></li>
-    <li><Link to="/">Contact</Link></li>
-  </>
+  const menuItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/">Services</Link>
+      </li>
+      <li>
+        <Link to="/">Page</Link>
+      </li>
+      <li>
+        <Link to="/">Courses</Link>
+      </li>
+      <li>
+        <Link to="/">News</Link>
+      </li>
+      <li>
+        <Link to="/">About</Link>
+      </li>
+      <li>
+        <Link to="/">Contact</Link>
+      </li>
+    </>
+  );
 
   return (
-    <div>
-      <div className="navbar bg-base-100 w-11/12 mx-auto">
-        <div className="navbar-start">
+    <div className='w-11/12 mx-auto'>
+      <div className="navbar flex justify-between mt-3 mb-[-5px]">
+        <div className="">  
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              {menuItems}</ul>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {menuItems}
+            </ul>
           </div>
           {/* <img src={nerd} alt="" /> */}
-          <Link href="/" className="font-bold"><span className="italic text-3xl">Nerd</span><span className="text-cyan-500 text-2xl"> ACADEMY</span></Link>
+          <div className='w-[210px]'>
+            <Link href="/" className="font-bold">
+            <span className="italic text-3xl">Nerd</span>
+            <span className="text-cyan-500 text-2xl"> ACADEMY</span>
+          </Link>
+          </div>
+          
+          
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className=" menu-horizontal gap-x-3">
-            {menuItems}
+
+        <div className="hidden lg:flex">
+          <ul className="menu menu-horizontal bg-white hover:bg-white">
+            <li>
+              <span className="hover:text-primary">Categories</span>
+              <ul className="bg-base-100 p-2 ">{menuItems}</ul>
+            </li>
           </ul>
         </div>
-        <div className="navbar-end">
-          {/* <a className="btn">Get started</a> */}
+        
+
+        <div class="input w-full">
+          
+          <input
+            type="text"
+            placeholder="Search for anything"
+            className="input input-bordered input-primary rounded-full w-full"
+          />
+        </div>
+      <div className='text-3xl hover:text-primary cursor-pointer mx-5'>
+        <FaOpencart />
+      </div>
+      
+
+        <div className="">
+            <button className='btn border-primary hover:border-primary hover:text-white bg-white hover:bg-primary text-black rounded-none mr-1'>Log in</button>
+            <button className='btn border-primary bg-primary text-white text rounded-none'>Sign up</button>
         </div>
       </div>
     </div>
