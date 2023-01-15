03d63e7caa7787662c2e0ce124c01b9356222931
Farhan Hoque <farhan@Farhans-MacBook-Pro.local>
2023-01-15 23:03:58 +0600

    checking auto hosting

---
+++ src/Routes/Routes.js
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import About from "../pages/About/About";
+import Contact from "../pages/Contact/Contact";
+import Courses from "../pages/Courses/Courses";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+            ,
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            }
+            ,
+            {
+                path: '/about',
+                element: <About></About>
+            }
+            ,
+            {
+                path: '/contact',
+                element: <Contact></Contact>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+        ]
+    }
+])
