41338bd785fb94a5eb656968ef712996238ffe18
Mamun2k22 <mamun180037@gmail.com>
2023-01-17 00:46:06 +0600

    Improvement the Counter

--- src/pages/Counter/Counter.js
+++ src/pages/Counter/Counter.js
@@ -9,64 +9,68 @@ const Counter = () => {
     // console.log(data);
     return (
 
-        <div>
-            <div className='grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-10 my-16 w-12/12 mx-8'>
-                <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <div className="flex justify-end px-4 pt-4">
 
+        <div style={{
+            background: `url(https://img.freepik.com/free-vector/blue-fluid-background_53876-114597.jpg?w=740&t=st=1673891733~exp=1673892333~hmac=f0fa97036719bb019b3d5970ca6bc9d368e91490bf1e3096d3365538f8cb4e92)`,
 
-                    </div>
-                    <div className="flex flex-col items-center pb-10">
-                        <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={users} alt='' />
-                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">USERS</h5>
-                        <span className="text-black-500 dark:text-gray-400 text-md font-semibold">60489</span>
+        }}
+            className='grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-10 my-28 w-12/12 mx-8'>
+            <div className="w-full max-w-sm  rounded-md shadow-md border border-indigo-700">
+                <div className="flex justify-end px-4 pt-4">
 
-                    </div>
-                </div>
-
-                <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <div className="flex justify-end px-4 pt-4">
-
-                    </div>
-                    <div className="flex flex-col items-center pb-10">
-                        <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={instructorf} alt='' />
-                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">INSTRUCTOR
-                        </h5>
-                        <span className=" text-black-500 dark:text-gray-400 text-md font-semibold">200</span>
-
-                    </div>
-                </div>
-                <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <div className="flex justify-end px-4 pt-4">
-
-
-                    </div>
-                    <div className="flex flex-col items-center pb-10">
-                        <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={learner} alt='' />
-                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">LEARNERS</h5>
-                        <span className="text-black-500 dark:text-gray-400 text-md font-semibold">500</span>
-
-                    </div>
 
                 </div>
-                <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <div className="flex justify-end px-4 pt-4">
-
-
-                    </div>
-                    <div className="flex flex-col items-center pb-10">
-                        <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={course} alt='' />
-                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">COURSES</h5>
-                        <span className="text-black-500 dark:text-gray-400 text-md font-semibold">150</span>
-
-                    </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg border border-zinc-50 my-4" src={users} alt='' />
+                    <h5 className="mb-1 text-xl font-bold text-gray-900 dark:text-white">USERS</h5>
+                    <span className="text-black-500 dark:text-gray-400 text-md font-semibold">60489</span>
 
                 </div>
+            </div>
+
+            <div className="w-full max-w-sm  rounded-md shadow-md border border-indigo-700 ">
+                <div className="flex justify-end px-4 pt-4">
+
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg my-4 border border-zinc-50 " src={instructorf} alt='' />
+                    <h5 className="mb-1 text-xl font-bold text-gray-900 dark:text-white">INSTRUCTOR
+                    </h5>
+                    <span className=" text-black-500 dark:text-gray-400 text-md font-semibold">200</span>
+
+                </div>
+            </div>
+            <div className="w-full max-w-sm  rounded-md shadow-md border border-slate-700">
+                <div className="flex justify-end px-4 pt-4">
 
 
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg my-4 border border-zinc-50" src={learner} alt='' />
+                    <h5 className="mb-1 text-xl font-bold">LEARNERS</h5>
+                    <span className="text-black-500 dark:text-gray-400 text-md font-semibold">500</span>
+
+                </div>
 
             </div>
+            <div className="w-full max-w-sm  rounded-md shadow-md border border-indigo-700">
+                <div className="flex justify-end px-4 pt-4">
+
+
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg my-4 border border-zinc-50" src={course} alt='' />
+                    <h5 className="mb-1 text-xl font-bold text-gray-900 dark:text-white">COURSES</h5>
+                    <span className="text-black-500 dark:text-gray-400 text-md font-semibold">150</span>
+
+                </div>
+
+            </div>
+
+
+
         </div>
+
     );
 };
 
