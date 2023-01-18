6719f69a314dab46f578f05787d34abbc2b20988
ahm-rubayed <rubayedahmed04@gmail.com>
2023-01-18 06:53:32 +0600

    banner updated

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import Courses from '../Courses/Courses'
+import Banner from '../home/Banner/Banner'
 import Counter from '../Counter/Counter';
 import OurCourses from './OurCourses/OurCourses';
 import Instructor from './Instructor/Instructor';
 import Contact from '../Contact/Contact';
 import About from '../About/About';
 import Faq from '../../Faq/Faq';
-import Banner from '../home/Banner/Banner'
 
 
 const Home = () => {
     return (
         <div>
-            <Banner/>
-            <Courses/>
-            <About/>
-            <OurCourses/>
-            <Counter/>
-            <Contact/>
-            <Faq/>
-            <Instructor/>
+            <Banner></Banner>
+            <OurCourses></OurCourses>
+            <Counter></Counter>
+            <About></About>
+            <Contact></Contact>
+            <Faq></Faq>
+            <Instructor></Instructor>
+
         </div>
     );
 };
