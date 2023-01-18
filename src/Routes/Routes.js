93851a32b9c451b2461229ebff056fa6ec07137b
mehedi556 <mehe556di@gmail.com>
2023-01-18 15:56:39 +0600

    added payment route and construction by mehedi

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import About from "../pages/About/About";
 import Cart from "../pages/Cart/Cart";
+import Checkout from "../pages/Checkout/Checkout";
 
 import Contact from "../pages/Contact/Contact";
 import CourseDetails from "../pages/Courses/CourseDetails";
@@ -10,6 +11,7 @@ import SummaryReview from "../pages/Courses/SummaryRoute.js/SummaryReview";
 import Home from "../pages/home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import UnderConstruction from "../pages/UnderConstruction/UnderConstruction";
 
 export const routes = createBrowserRouter([
     {
@@ -47,10 +49,21 @@ export const routes = createBrowserRouter([
             {
                 path: '/register',
                 element: <Register></Register>
-            },
+            }
+            ,
             {
                 path: '/cart',
                 element: <Cart></Cart>
+            }
+            ,
+            {
+                path: '/checkout',
+                element: <Checkout></Checkout>
+            }
+            ,
+            {
+                path: '/construction',
+                element: <UnderConstruction></UnderConstruction>
             }
             
         ]
