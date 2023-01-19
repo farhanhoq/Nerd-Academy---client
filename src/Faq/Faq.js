ecce3cb9f616881e3b80546542e9290ac6c7d9fa
neasher <neasher75@gmail.com>
2023-01-19 19:40:38 +0600

    responsive faq

--- src/Faq/Faq.js
+++ src/Faq/Faq.js
@@ -25,8 +25,8 @@ const Faq = () => {
                     </p>
                 </div>
 
-                <div class="flex lg:w-4/5 sm:mx-auto sm:mb-2 flex-row-reverse items-center gap-6">
-                    <div class=" flex flex-col w-full mr-40">
+                <div class="flex lg:w-4/5 mx-auto mb-2 flex-col lg:flex-row-reverse items-center justify-around gap-6">
+                    <div class="lg:w-full w-11/12 lg:mr-40">
                         <img className='hue-rotate-0 w-full justify-center rounded-md shadow-gray-400' src="https://img.freepik.com/free-vector/organic-flat-people-business-training_23-2148896824.jpg?w=740&t=st=1673904517~exp=1673905117~hmac=0c55fb2d4a5b1324d41bc752c767b2f92b970f1b81548a9c6c5394dfb74d088f" alt="" srcset="" />
                     </div>
 
