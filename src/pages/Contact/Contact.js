1f046cd6b793b7ed5e38e9a54c2c479980f23497
Mamun2k22 <mamun180037@gmail.com>
2023-01-19 17:38:46 +0600

    Some modification contact section

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,23 +2,22 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 justify-around items-center mt-40 px-16" id="contact">
+    <section className="flex flex-row  justify-center items-center lg:flex-row mt-40 container mx-auto px-16" id="contact">
       <div className="text-center lg:text-left">
         <img
-          className="w-11/12"
+          className="w-10/12"
           src="https://img.freepik.com/free-vector/flat-design-illustration-customer-support_23-2148887720.jpg?w=740&t=st=1673781447~exp=1673782047~hmac=c4925cf02c78fea815b894b4393247cf7e71d96a32a1f3f2596131ec8b36c070"
           alt=""
         />
       </div>
 
-      <div className="flex flex-col  mr-20 justify-self-center">
-        <h2 className="mb-6 text-center lg:text-left text-2xl font-bold">
+      <div className="grid justify-between mt-10 mr-20 justify-self-center">
+        <h2 className="text-center lg:text-left text-2xl font-bold">
           {" "}
           Stay Connected with us
           {" "}
         </h2>
 
-<<<<<<< HEAD
         {/* From section */}
         <form
           action="https://formspree.io/f/mzbqzjbb"
@@ -61,15 +60,6 @@ const Contact = () => {
             />
           </div>
         </form>
-=======
-        <div className="mb-16">
-          <input
-            type="submit"
-            value="Send Message"
-            className="mt-6 btn btn-primary bg-gradient-to-r from-primary to secondary w-40 rounded-full text-white"
-          />
-        </div>
->>>>>>> 09f1d921d321c9245629d54b36565e6b93e1d96d
       </div>
     </section>
   );
