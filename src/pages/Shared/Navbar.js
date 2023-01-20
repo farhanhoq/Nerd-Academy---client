6b9b3575a4bd5134a75c9747568bc090ac667f32
Mamun2k22 <mamun180037@gmail.com>
2023-01-20 15:20:15 +0600

    Changes the FAQ section modify, and rendering Contact and Instructor

--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -8,7 +8,7 @@ import { useQuery } from "@tanstack/react-query";
 
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
-  const [filteredData , setFilteredData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const { user, logOut } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
@@ -16,108 +16,114 @@ const Navbar = () => {
   const { data: coursesData = [], isLoading, refetch } = useQuery({
     queryKey: ['coursesData'],
     queryFn: () => fetch('https://nerd-academy-server.vercel.app/courses')
-        .then(res => res.json())
-});
-
-const handleLogOut = () => {
-  logOut()
-    .then(() => {
-      toast.success("Sign Out Successfully");
-      navigate('/');
-    })
-    .catch(error => {
-      toast.error(error.message);
-    })
-}
-
-const changeBg = () => {
-  if (window.scrollY >= 20) {
-    setNavbar(true)
-  }
-  else {
-    setNavbar(false)
-  }
-}
-
-window.addEventListener('scroll', changeBg)
-const menuItems = (
-  <>
-    <li>
-      <a href="/#home">Home</a>
-    </li>
-    <li>
-      <a href="/#about">About</a>
-    </li>
-    <li>
-      <a href="/#courses">Courses</a>
-    </li>
-    <li>
-      <a href="/details">Courses Details</a>
-    </li>
-    <Link to='/construction'>
-      <li>
-        <a href="/#news">Blog</a>
-      </li>
-    </Link>
-    <li>
-      <a href="/#contact">Contact</a>
-    </li>
-    <li className="block lg:hidden">
-      <a href="/login">Login</a>
-    </li>
-    <li className="block lg:hidden">
-      <a href="/register">Register</a>
-    </li>
-
-  </>
-);
-
-const handleFilter = (e) => {
-  const searchedWord = e.target.value;
-  const newFilter = coursesData.filter(value => {
-    return value.title.toLowerCase().includes(searchedWord.toLowerCase());
+      .then(res => res.json())
   });
 
-  if(searchedWord === ""){
-    setFilteredData("")
-  }else{
-    setFilteredData(newFilter);
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        toast.success("Sign Out Successfully");
+        navigate('/');
+      })
+      .catch(error => {
+        toast.error(error.message);
+      })
   }
-}
+
+  const changeBg = () => {
+    if (window.scrollY >= 20) {
+      setNavbar(true)
+    }
+    else {
+      setNavbar(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeBg)
+  const menuItems = (
+    <>
+      <li>
+        <a href="/#home">Home</a>
+      </li>
+      <li>
+        <a href="/#about">About</a>
+      </li>
+      <li>
+        <a href="/#courses">Courses</a>
+      </li>
+      <Link to='/construction'>
+        <li>
+          <a href="/#news">Blog</a>
+        </li>
+      </Link>
+      {/* Conditional Rendering */}
+      {
+        user?.uid ?
+          <li>
+            <a href="/#contact">Contact</a>
+          </li>
+          :
+          <>
+
+          </>
+      }
+
+      <li className="block lg:hidden">
+        <a href="/login">Login</a>
+      </li>
+      <li className="block lg:hidden">
+        <a href="/register">Register</a>
+      </li>
+
+    </>
+  );
+
+  const handleFilter = (e) => {
+    const searchedWord = e.target.value;
+    const newFilter = coursesData.filter(value => {
+      return value.title.toLowerCase().includes(searchedWord.toLowerCase());
+    });
+
+    if (searchedWord === "") {
+      setFilteredData("")
+    } else {
+      setFilteredData(newFilter);
+    }
+  }
 
 
 
   return (
     <nav className={navbar ? "navbar active flex justify-between w-full mx-auto fixed z-10 px-16" : "navbar flex justify-between w-full mx-auto fixed z-10 px-16"}>
-      
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {menuItems}
-          </ul>
-        </div>
 
-        <div class="hidden lg:block w-[30%]">
-          
-          
+      <div className="dropdown">
+        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+        <ul
+          tabIndex={0}
+          className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          {menuItems}
+        </ul>
+      </div>
+
+      <div class="hidden lg:block w-[30%]">
+
+
 
         {/* <img src={nerd} alt="" /> */}
         <div className="w-[210px]">
@@ -142,23 +148,23 @@ const handleFilter = (e) => {
       </div>
 
       <div class="hidden lg:block w-[30%]">
-      <input
-            type="text"
-            placeholder="Search courses here"
-            className="input input-bordered input-primary rounded-full w-full"
-            onChange={handleFilter}
-          />
-          {
-            filteredData.length !== 0 && (
-              <div className="dataResult mt-[5px] w-11/12 mx-auto rounded-md bg-white border z-20 border-primary">
-            {
-              filteredData?.slice(0, 5).map(( value , key ) => {
-                return <Link key={key} className="w-full h-[50px] flex items-center pl-4 hover:bg-primary hover:text-white" to={`/details/${value?._id}`}><p>{value?.title}</p></Link>
-              })
-            }
-          </div>
-            )
-          }
+        <input
+          type="text"
+          placeholder="Search courses here"
+          className="input input-bordered input-primary rounded-full w-full"
+          onChange={handleFilter}
+        />
+        {
+          filteredData.length !== 0 && (
+            <div className="dataResult mt-[5px] w-11/12 mx-auto rounded-md bg-white border z-20 border-primary">
+              {
+                filteredData?.slice(0, 5).map((value, key) => {
+                  return <Link key={key} className="w-full h-[50px] flex items-center pl-4 hover:bg-primary hover:text-white" to={`/details/${value?._id}`}><p>{value?.title}</p></Link>
+                })
+              }
+            </div>
+          )
+        }
       </div>
       <div className='text-3xl hover:text-primary cursor-pointer mx-5'>
         <Link to='/cart'><FaOpencart /></Link>
@@ -166,6 +172,7 @@ const handleFilter = (e) => {
 
 
       <div>
+
         {
           user?.uid ?
             <Link onClick={handleLogOut} className="btn border-primary hover:border-primary hover:text-white bg-white hover:bg-primary text-black 
@@ -186,7 +193,13 @@ const handleFilter = (e) => {
                 Sign up
               </Link>
             </>
+
         }
+
+
+
+
+
 
       </div>
     </nav>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import Banner from '../home/Banner/Banner'
+import React, { useContext } from 'react';
+import Banner from '../Home/Banner/Banner'
 import Counter from '../Counter/Counter';
 import OurCourses from './OurCourses/OurCourses';
 import Instructor from './Instructor/Instructor';
 import Contact from '../Contact/Contact';
 import About from '../About/About';
 import Faq from '../../Faq/Faq';
+import { AuthContext } from '../../Context/AuthProvider';
 
 
 const Home = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <div>
             <Banner></Banner>
             <OurCourses></OurCourses>
             <Counter></Counter>
             <About></About>
-            <Contact></Contact>
+            {
+                user?.uid ?
+                    <Contact></Contact>
+                    :
+                    <>
+                    </>
+            }
+
             <Faq></Faq>
-            <Instructor></Instructor>
+            {
+                user?.uid ?
+                    <>
+
+                    </>
+                    :
+                    <Instructor></Instructor>
+
+            }
+
         </div>
     );
 };
