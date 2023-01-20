5e173c7f245cae5a269f98fe903aa2d4299d210d
neasher <neasher75@gmail.com>
2023-01-20 12:24:59 +0600

    save users to db

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -48,7 +48,7 @@ const Register = () => {
       role: account
     }
 
-    fetch('http://localhost:5000/users', {
+    fetch('https://nerd-academy-server.vercel.app/users', {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
