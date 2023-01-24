09b48405265f90549dd7e1dd231b37daa1dd385c
Mamun2k22 <mamun180037@gmail.com>
2023-01-24 10:30:30 +0600

    Resize vector image size

--- src/pages/Home/Instructor/Instructor.js
+++ src/pages/Home/Instructor/Instructor.js
@@ -7,7 +7,7 @@ const Instructor = () => {
         <div className='flex flex-col md:flex-row items-center w-11/12 mx-auto shadow-2xl my-28 rounded-xl'>
 
             <div className='w-full md:w-6/12'>
-                <img className='rounded-tl-md rounded-bl-md' src="https://img.freepik.com/premium-vector/business-people-having-board-meeting_1034-1214.jpg" alt="instructor" />
+                <img className='rounded-tl-md rounded-bl-md w-9/12' src="https://img.freepik.com/premium-vector/business-people-having-board-meeting_1034-1214.jpg?w=740" alt="instructor" />
             </div>
             <div className='md:w-6/12 p-5 md:ml-5'>
 
