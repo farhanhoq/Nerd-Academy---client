f77399a81bd6aaefab7cd4c94d28a498c08c3429
Mamun2k22 <mamun180037@gmail.com>
2023-01-24 17:57:04 +0600

    changes counter and img vector

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -8,7 +8,6 @@ import CourseDetails from "../pages/Courses/CourseDetails";
 import Review from "../pages/Courses/Review";
 import Home from "../pages/Home/Home";
 import Error from "../pages/Error/Error";
-import Home from '../pages/home/Home'
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import UnderConstruction from "../pages/UnderConstruction/UnderConstruction";
--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -5,8 +5,6 @@ import "./Navbar.css";
 import { AuthContext } from "../../Context/AuthProvider";
 import { toast } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
-import DarkLightMode from "../../Theme-Change/DarkLightMode";
-import ThemeChanger from "../../Theme-Change/ThemeChanger";
 
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
@@ -17,15 +15,15 @@ const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("light");
 
-const toggleTheme = () => {
-  setTheme(theme === "night" ? "light" : "night");
-};
+  const toggleTheme = () => {
+    setTheme(theme === "night" ? "light" : "night");
+  };
 
- React.useEffect(() => {
-   document.querySelector("html").setAttribute("data-theme", theme);
- }, [theme]);
+  React.useEffect(() => {
+    document.querySelector("html").setAttribute("data-theme", theme);
+  }, [theme]);
 
   const {
     data: coursesData = [],
