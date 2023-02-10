aa96544edcc6bfdb36e3dc0890d5109d769c1a77
Mamun2k22 <mamun180037@gmail.com>
2023-02-10 16:26:47 +0600

    Changes pending and Publish

--- src/pages/Dashboard/Instructor/MyCourse.js
+++ src/pages/Dashboard/Instructor/MyCourse.js
@@ -26,7 +26,7 @@ const MyCourse = () => {
                             <li>
                                 <Link to="/dashboard/publish" class="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
                                     <img className='h-5 w-5' src="https://cdn-icons-png.flaticon.com/512/9628/9628230.png" alt="" srcset="" />
-                                    <span class="tab tab-bordered group-hover:text-gray-700">Publish</span>
+                                    <span class="tab tab-bordered tab-active group-hover:text-gray-700">Publish</span>
                                     {/* <Link to="/dashboard/profile"><span class="group-hover:text-gray-700">Profile</span></Link> */}
                                 </Link>
                             </li>
