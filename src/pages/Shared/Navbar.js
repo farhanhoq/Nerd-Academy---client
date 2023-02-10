f24dca1113d688cfd2adab4d73cea1c540529fc6
mehedi556 <mehe556di@gmail.com>
2023-02-10 20:50:05 +0600

    fixing bugs in searchbar

--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -228,7 +228,7 @@ const Navbar = () => {
       <div className="hidden lg:block w-[45%] ">
         <ul className="menu menu-horizontal w-full">
           <li tabIndex={0} className="w-full">
-            <a className="bg-transparent w-full" href="/">
+            <a className="bg-transparent w-full">
               <input
                 type="text"
                 placeholder="Search courses here"
