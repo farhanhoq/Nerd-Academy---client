ad3dd5c9666f890a5d60d1066cb23b18953ab863
ahm-rubayed <rubayedahmed04@gmail.com>
2023-02-17 18:49:55 +0600

    asdf

--- src/pages/Dashboard/AdminDashboard/AdminDashboard.js
+++ src/pages/Dashboard/AdminDashboard/AdminDashboard.js
@@ -1,157 +1,178 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const res = await fetch(
+        `https://nerd-academy-server.vercel.app/all-users`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+  // console.log(users.length);
 
-    const { data: users = [], refetch } = useQuery({
-        queryKey: ["user"],
-        queryFn: async () => {
-            const res = await fetch(`https://nerd-academy-server.vercel.app/all-users`);
-            const data = await res.json();
-            return data;
-        }
-    });
-    // console.log(users.length);
+  const { data: courses = [] } = useQuery({
+    queryKey: ["courses"],
+    queryFn: async () => {
+      const res = await fetch(`https://nerd-academy-server.vercel.app/courses`);
+      const data = await res.json();
+      return data;
+    },
+  });
+  // console.log(courses);
 
-    const { data: courses = [] } = useQuery({
-        queryKey: ["courses"],
-        queryFn: async () => {
-            const res = await fetch(`https://nerd-academy-server.vercel.app/courses`);
-            const data = await res.json();
-            return data;
-        }
-    });
-    // console.log(courses);
+  const { data: perchasedCourse = [] } = useQuery({
+    queryKey: ["perchasedCourse"],
+    queryFn: async () => {
+      const res = await fetch(
+        `https://nerd-academy-server.vercel.app/perchased-course`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
 
-    const { data: perchasedCourse = [] } = useQuery({
-        queryKey: ["perchasedCourse"],
-        queryFn: async () => {
-            const res = await fetch(`https://nerd-academy-server.vercel.app/perchased-course`);
-            const data = await res.json();
-            return data;
-        }
-    });
+  const { data: countAPI = [] } = useQuery({
+    queryKey: ["countAPI"],
+    queryFn: async () => {
+      const res = await fetch(
+        `https://api.countapi.xyz/update/neasher/neasher1/?amount=1`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
 
-    const { data: countAPI = [] } = useQuery({
-        queryKey: ["countAPI"],
-        queryFn: async () => {
-            const res = await fetch(`https://api.countapi.xyz/update/neasher/neasher1/?amount=1`);
-            const data = await res.json();
-            return data;
-        }
-    });
+  refetch();
+  // console.log(countAPI);
 
-    refetch();
-    // console.log(countAPI);
+  return (
+    <div className="mx-auto">
+      <h2 className="mt-12 mb-6 text-4xl font-bold text-primary text-center">
+        Welcome to Admin Dashboard
+      </h2>
 
-    return (
-        <div className='mx-auto'>
-            <h2 className='mt-12 mb-6 text-4xl font-bold text-primary text-center'>Welcome to Admin Dashboard</h2>
-
-            <div className="card bg-base-100 shadow-xl pt-12">
-
-                <div className="bg-gray-900 opacity-50 hidden" id="sidebarBackdrop"></div>
-                <div id="main-content" className="h-full w-full bg-gray-50 relative overflow-y-auto">
-                    <main>
-                        <div className="pt-6 px-4">
-                            <div className="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
-
-
-                            </div>
-                            <div className="mt-4 w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-4 xl:p-8 ">
-                                    <div className="flex items-center">
-                                        <div className="flex-shrink-0">
-                                            <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">{courses?.length}</span>
-                                            <h3 className="text-base font-normal text-gray-500">Total Courses</h3>
-                                        </div>
-                                        {/* <div className="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
+      <div className="card bg-base-100 shadow-xl pt-12">
+        <div
+          className="bg-gray-900 opacity-50 hidden"
+          id="sidebarBackdrop"
+        ></div>
+        <div
+          id="main-content"
+          className="h-full w-full bg-gray-50 relative overflow-y-auto"
+        >
+          <main>
+            <div className="pt-6 px-4">
+              <div className="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4"></div>
+              <div className="mt-4 w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+                <div className="bg-white shadow rounded-lg p-4 sm:p-4 xl:p-8 ">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                      <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">
+                        {courses?.length}
+                      </span>
+                      <h3 className="text-base font-normal text-gray-500">
+                        Total Courses
+                      </h3>
+                    </div>
+                    {/* <div className="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
                                             14.6%
                                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                 <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
                                             </svg>
                                         </div> */}
-                                    </div>
-                                </div>
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-                                    <div className="flex items-center">
-                                        <div className="flex-shrink-0">
-                                            <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">{countAPI.value}</span>
-                                            <h3 className="text-base font-normal text-gray-500">Visitors this week</h3>
-                                        </div>
-                                        {/* <div className="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
+                  </div>
+                </div>
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                      <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">
+                        {countAPI.value}
+                      </span>
+                      <h3 className="text-base font-normal text-gray-500">
+                        Visitors this week
+                      </h3>
+                    </div>
+                    {/* <div className="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
                                             32.9%
                                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                 <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
                                             </svg>
                                         </div> */}
-                                    </div>
-                                </div>
-                                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-                                    <div className="flex items-center">
-                                        <div className="flex-shrink-0">
-                                            <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">{users?.length}</span>
-                                            <h3 className="text-base font-normal text-gray-500">User SignUps</h3>
-                                        </div>
-                                        {/* <div className="ml-5 w-0 flex items-center justify-end flex-1 text-red-500 text-base font-bold">
+                  </div>
+                </div>
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                      <span className="text-2xl sm:text-3xl leading-none font-bold text-gray-900">
+                        {users?.length}
+                      </span>
+                      <h3 className="text-base font-normal text-gray-500">
+                        User SignUps
+                      </h3>
+                    </div>
+                    {/* <div className="ml-5 w-0 flex items-center justify-end flex-1 text-red-500 text-base font-bold">
                                             -2.7%
                                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                 <path fill-rule="evenodd" d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
                                             </svg>
                                         </div> */}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="w-full my-4">
-                                <div className="bg-white shadow rounded-lg mb-4 p-4 sm:p-6 h-full">
-                                    <div className="flex items-center justify-between mb-4">
-                                        <h3 className="text-xl font-bold leading-none text-gray-900">Latest Customers</h3>
-                                        <hr />
-                                        {/* <a href="/" className="text-sm font-medium text-cyan-600 hover:bg-gray-100 rounded-lg inline-flex items-center p-2">
+                  </div>
+                </div>
+              </div>
+              <div className="w-full my-4">
+                <div className="bg-white shadow rounded-lg mb-4 p-4 sm:p-6 h-full">
+                  <div className="flex items-center justify-between mb-4">
+                    <h3 className="text-xl font-bold leading-none text-gray-900">
+                      Latest Customers
+                    </h3>
+                    <hr />
+                    {/* <a href="/" className="text-sm font-medium text-cyan-600 hover:bg-gray-100 rounded-lg inline-flex items-center p-2">
                                             View all
                                         </a> */}
-                                    </div>
-                                    <div className="flow-root">
-                                        <ul className='divide-y divide-gray-200'>
-
-                                            {
-                                                perchasedCourse.map(customer => <li key={customer?._id} className="py-3 sm:py-4">
-                                                    <div className="flex items-center space-x-4">
-                                                        <div className="flex-shrink-0">
-                                                            <img className='h-8 w-8 rounded-full' src={customer?.picture} alt="customer" />
-                                                        </div>
-                                                        <div className="flex-1 min-w-0">
-                                                            <p className="text-sm font-medium text-gray-900 truncate">
-                                                                {customer?.title}
-                                                            </p>
-                                                            <p className="text-sm text-gray-500 truncate">
-                                                                {customer?.buyerEmail}
-                                                            </p>
-                                                        </div>
-                                                        <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                                            ${customer?.price}
-                                                        </div>
-                                                    </div>
-                                                </li>)
-                                            }
-
-
-
-                                        </ul>
-                                    </div>
-                                </div>
+                  </div>
+                  <div className="flow-root">
+                    <ul className="divide-y divide-gray-200">
+                      {perchasedCourse.map((customer) => (
+                        <li key={customer?._id} className="py-3 sm:py-4">
+                          <div className="flex items-center space-x-4">
+                            <div className="flex-shrink-0">
+                              <img
+                                className="h-8 w-8 rounded-full"
+                                src={customer?.picture}
+                                alt="customer"
+                              />
                             </div>
-                        </div>
-                    </main>
-
+                            <div className="flex-1 min-w-0">
+                              <p className="text-sm font-medium text-gray-900 truncate">
+                                {customer?.title}
+                              </p>
+                              <p className="text-sm text-gray-500 truncate">
+                                {customer?.buyerEmail}
+                              </p>
+                            </div>
+                            <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                              ${customer?.price}
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
+              </div>
             </div>
-            <script async defer src="https://buttons.github.io/buttons.js"></script>
-            <script src="https://demo.themesberg.com/windster/app.bundle.js"></script>
-
+          </main>
         </div>
-    );
+      </div>
+      <script async defer src="https://buttons.github.io/buttons.js"></script>
+      <script src="https://demo.themesberg.com/windster/app.bundle.js"></script>
+    </div>
+  );
 };
 
 export default AdminDashboard;
