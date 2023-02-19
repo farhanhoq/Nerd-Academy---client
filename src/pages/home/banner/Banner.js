10f3096221da4e1ab275a0de7359822ecc0886b5
Farhan Hoque <fhrubik98@gmail.com>
2023-02-19 08:10:17 +0600

    resolve warning in banner.js

--- src/pages/home/banner/Banner.js
+++ src/pages/home/banner/Banner.js
@@ -5,32 +5,49 @@ import { Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
 const Banner = () => {
-
+  
+  /*eslint-disable */
   const { data: countAPI = [] } = useQuery({
     queryKey: ["countAPI"],
     queryFn: async () => {
-      const res = await fetch(`https://api.countapi.xyz/update/neasher/neasher1/?amount=1`);
+      const res = await fetch(
+        `https://api.countapi.xyz/update/neasher/neasher1/?amount=1`
+      );
       const data = await res.json();
       return data;
-    }
+    },
   });
+  /*eslint-enable */
 
   return (
-    <div className="hero min-h-screen grid-cols-2 relative" style={{ backgroundImage: `url(${newBg})` }} id="#home">
-      <div className='absolute left-[50px] top-[80px]'>
+    <div
+      className="hero min-h-screen grid-cols-2 relative"
+      style={{ backgroundImage: `url(${newBg})` }}
+      id="#home"
+    >
+      <div className="absolute left-[50px] top-[80px]">
         <div className="hero-content items-center justify-center flex-col md:flex-row">
-          <div className='w-full text-white mx-auto'>
+          <div className="w-full text-white mx-auto">
             <h1 className="text-4xl font-bold">Online Learning Platform!</h1>
-            <p className="py-8 w-2/3">Nerd Academy is an online learning community based in the globally for people who want to learn from educational videos. The courses, which are not accredited, are only available through paid subscription.</p>
+            <p className="py-8 w-2/3">
+              Nerd Academy is an online learning community based in the globally
+              for people who want to learn from educational videos. The courses,
+              which are not accredited, are only available through paid
+              subscription.
+            </p>
             {/* <button className="btn text-white">Join For Free</button> */}
-            <Link to="#courses" className="btn bg-white dark:bg-black text-black dark:text-white border-none hover:bg-white hover:shadow-2xl shadow-white">Explore our courses</Link>
+            <Link
+              to="#courses"
+              className="btn bg-white dark:bg-black text-black dark:text-white border-none hover:bg-white hover:shadow-2xl shadow-white"
+            >
+              Explore our courses
+            </Link>
           </div>
-          <div className=''>
-            <img src={banner} alt="" className='hidden md:block' />
+          <div className="">
+            <img src={banner} alt="" className="hidden md:block" />
           </div>
         </div>
       </div>
-
     </div>
   );
 };
