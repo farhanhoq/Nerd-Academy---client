9b979f03b793cc9a086547742e55b7748b38804e
ahm-rubayed <rubayedahmed04@gmail.com>
2023-02-20 15:47:14 +0600

    Teacher dashboard counter added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
     themes: [
       {
         nerdthemes: {
-          primary: '#7B33FD',
-          secondary: '#B337E3',
+          primary: '#4568dc',
+          secondary: '#b06ab3',
           accent: "#3A4256",
           neutral: "#3D4451",
           "base-100": "#FFFFFF",
