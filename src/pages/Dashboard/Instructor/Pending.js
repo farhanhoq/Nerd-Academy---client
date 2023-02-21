9a0fef97e6c04bbb67402367fb32fa7b44030736
ahm-rubayed <rubayedahmed04@gmail.com>
2023-02-21 09:10:48 +0600

    asdf

--- src/pages/Dashboard/Instructor/Pending.js
+++ src/pages/Dashboard/Instructor/Pending.js
@@ -119,52 +119,26 @@ const Pending = () => {
                                                     </svg>
                                                 </div> */}
                                                 </div>
-<<<<<<< HEAD
                                             </div >
                                             <div className="flex items-center relative  border-b border-primary py-1"></div>
                                             <div className="flex items-center relative my-2 justify-between">
-=======
-                                                <div className="flex items-center relative  border-b border-cyan-500 py-1"></div>
-        <div className="flex items-center relative my-2">
->>>>>>> 12be312fe797214d113133fe95bc19a50dff880f
 
-            <div className="ml-3">
-                <p className="font-medium text-gray-800">
-                    {p?.tutor}
-                </p>
-                <p className="text-md text-gray-600"></p>
-            </div>
+                                                <div className="ml-3">
+                                                    <p className="font-medium text-gray-800">
+                                                        {p?.tutor}
+                                                    </p>
+                                                    <p className="text-md text-gray-600"></p>
+                                                </div>
 
-<<<<<<< HEAD
-<div className="flex items-center justify-between space-x-1.5 rounded-lg px-4 py-1.5">
-    <Link to={`/details/${p._id}`} className="text-sm border-none text-white btn btn-sm bg-gradient-to-r from-primary to-secondary">
-        Edit
-    </Link>
+                                                <div className="flex items-center justify-between space-x-1.5 rounded-lg px-4 py-1.5">
+                                                    <Link to={`/details/${p._id}`} className="text-sm border-none text-white btn btn-sm bg-gradient-to-r from-primary to-secondary">
+                                                        Edit
+                                                    </Link>
 
-=======
-                                                    <div className="flex items-center justify-between space-x-1.5 rounded-lg px-4 py-1.5">
-        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-4 w-4">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                                </svg> */}
-
-        <Link to={`/details/${p._id}`} className="text-sm btn btn-sm">
-            Edit
-        </Link>
-
-    </div>
-    {/* <Link onClick={() => deleteProduct(p._id)}>
-                                                    <button class="items-center px-2 py-2 text-white bg-red-500 dark:bg-gray-500 rounded-md hover:bg-red-600 dark:hover:bg-gray-600 focus:outline-none">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
-                                                        </svg>
-                                                    </button>
-                                                </Link> */}
-
->>>>>>> 12be312fe797214d113133fe95bc19a50dff880f
-</div>
+                                                </div>
                                             </div >
                                         </div >
-    <PendingModal p={p}></PendingModal>
+                                        <PendingModal p={p}></PendingModal>
                                     </div >
 
                                 </>
