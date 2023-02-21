79d5c2af0ea8dd12fa81e956b82ca44c03b654a7
ahm-rubayed <rubayedahmed04@gmail.com>
2023-02-21 14:28:47 +0600

    video upload added

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getStorage} from "firebase/storage"
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,5 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
 
 export default app;
